04ab12723f0a3eeada5e9288cd37f0289466e1aa
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-09-03 01:30:20 -0400

    feat: esta va pa la Vanana

--- runStatements.sql
+++ runStatements.sql
@@ -4,12 +4,32 @@
 -- 		- PEDIDO -> CLiente -> Repartidor -> Medio de Transporte
 
 -- 1. Lista de clientes con más pedidos por compañía
--- Cliente -> Pedido -> Venta_Detalle -> Producto -> Compañia
--- 1) Sacar los pedidos de los clientes, a partir de esos pedidos, sacar los productos y a partir de esos productos obtener la compañia
-SELECT comp.nombre
+
+-- Aca puedo contar cuantos clientes han hecho pedidos a tales compañias
+SELECT count(pd.id_cliente) as cantidad, comp.nombre
 FROM public."Compania" as comp
 INNER JOIN public."Producto" as pro ON pro.id_compania = comp.id_compania
-INNER JOIN public."Venta_Detalle" as vd ON vd.id_producto = pro.id_producto;
+INNER JOIN public."Venta_Detalle" as vd ON vd.id_producto = pro.id_producto
+INNER JOIN public."Pedido" as pd ON pd.id_pedido = vd.id_pedido
+INNER JOIN public."Cliente" as cli ON cli.id_cliente = pd.id_cliente
+GROUP BY comp.nombre
+ORDER BY cantidad
+DESC;
 
+select * from "Compania";
+-- Según yo acá obtengo todos los clientes que han hecho pedidos y esos pedidos corresponden a tales compañias, sin embargo faltaria hacer un LIMIT
+-- para solamente obtener por ejemplo 5 filas (Que serian los que han hecho más pedidos)
+SELECT comp.nombre,cli.nombre
+FROM public."Compania" as comp
+INNER JOIN public."Producto" as pro ON pro.id_compania = comp.id_compania
+INNER JOIN public."Venta_Detalle" as vd ON vd.id_producto = pro.id_producto
+INNER JOIN public."Pedido" as pd ON pd.id_pedido = vd.id_pedido
+INNER JOIN public."Cliente" as cli ON cli.id_cliente = pd.id_cliente;
 
-select * from "Producto";
+SELECT * from "Venta_Detalle" where "Venta_Detalle".fecha >= NOW() - INTERVAL '1 YEAR' and "Venta_Detalle".fecha < NOW();
+-- 8. Lista de compañías que han recibido más ingresos en el ultimo año
+SELECT *
+FROM public."Compania" as comp
+INNER JOIN public."Producto" as pro ON pro.id_compania = comp.id_compania
+INNER JOIN public."Venta_Detalle" as vent ON vent.id_producto = pro.id_producto
+where vent.fecha >= NOW() - INTERVAL '1 YEAR' and vent.fecha < NOW();
