fce33d1736a933203e888b30c6c83ad5a2af5878
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-09-05 03:18:10 -0300

    feat: deliverydb create script y respaldo

--- dbCreate.sql
+++ dbCreate.sql
@@ -1,4 +1,19 @@
+-------------------------------------------------------
+-- Crear DeliveryDB
+-- Database: DeliveryDB
 
+-- DROP DATABASE IF EXISTS "DeliveryDB";
+
+CREATE DATABASE "DeliveryDB"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Chile.1252'
+    LC_CTYPE = 'Spanish_Chile.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+-------------------------------------------------------
 -------------------------------------------------------
 --Table "Medio_transporte"
 -------------------------------------------------------
